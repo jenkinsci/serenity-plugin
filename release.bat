cc8a533534cfbc88a696475321060c54dd750d4b
michaelcouck <michaelcouck@dev.java.net>
2010-03-16 12:08:37 +0000

    Commit after the 0.3 release.

--- release.bat
+++ release.bat
@@ -1,4 +1,7 @@
-set MAVEN_OPTS=-Xms256m -Xmx1024m -XX:MaxPermSize=128m
+set MAVEN_OPTS=-Xms512m -Xmx1024m -XX:MaxPermSize=256m
+
 # mvn -DskipTests=true package -DdryRun=true
 # -B -Dresume=false -Dusername=michaelcouck -Dpassword=suineg
-mvn release:prepare release:perform
+# mvn release:rollback
+
+mvn -DskipTests=true -Dresume=false -DXms512m -DXmx1024m -DXX:MaxPermSize=256m release:prepare release:perform
