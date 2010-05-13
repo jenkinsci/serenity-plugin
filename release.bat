4fc9649bbb8355b0bd7520bbc775c844ff0532f2
michaelcouck <michaelcouck@dev.java.net>
2010-05-13 11:05:59 +0000

    Released version 0.4.

--- release.bat
+++ release.bat
@@ -1,7 +1,8 @@
 set MAVEN_OPTS=-Xms512m -Xmx1024m -XX:MaxPermSize=256m
 
-# mvn -DskipTests=true package -DdryRun=true
-# -B -Dresume=false -Dusername=michaelcouck -Dpassword=suineg
-# mvn release:rollback
-svn --non-interactive
+rem mvn -DskipTests=true package -DdryRun=true
+rem -B -Dresume=false -Dusername=michaelcouck -Dpassword=suineg
+rem mvn release:rollback
+rem svn --non-interactive
+
 mvn -DskipTests=true -Dresume=false -Dusername=michaelcouck -Dpassword=suineg -DXms512m -DXmx1024m -DXX:MaxPermSize=256m release:clean release:prepare release:perform
