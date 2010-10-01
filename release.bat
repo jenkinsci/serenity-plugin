70b076466a6006530617ce6d61544b6fcf6dd84b
michaelcouck <michaelcouck@dev.java.net>
2010-10-01 12:28:25 +0000

    Removing build bat.

--- release.bat
+++
@@ -1,8 +0,0 @@
-set MAVEN_OPTS=-Xms512m -Xmx1024m -XX:MaxPermSize=256m
-
-rem mvn -DskipTests=true package -DdryRun=true
-rem -B -Dresume=false -Dusername=michaelcouck -Dpassword=suineg
-rem mvn release:rollback
-rem svn --non-interactive
-
-mvn -DskipTests=true -Dresume=false -Dusername=michaelcouck -Dpassword=suineg -DXms512m -DXmx1024m -DXX:MaxPermSize=256m release:clean release:prepare release:perform
